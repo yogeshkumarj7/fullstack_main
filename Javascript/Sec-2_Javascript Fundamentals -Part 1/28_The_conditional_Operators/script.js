6d944f3430791b4fa579e019fdd9874bc4975b26
y09e5h <coolcoder9334@gmail.com>
2021-10-02 15:42:52 +0530

    :

--- Javascript/Sec-2_Javascript Fundamentals -Part 1/28_The_conditional_Operators/script.js
+++
@@ -1,5 +0,0 @@
-//conditional operator
-
-const age=17;
-const info=age>=18 ? "u can vote " : "ur not eligible for voting ";
-console.log(info)
--- Javascript/Sec-2_Javascript Fundamentals -Part 1/29_Coding_Challenge_4/script.js
+++
@@ -1,4 +0,0 @@
-const bill=275;
-const tip=bill<=300 && bill>=50 ? bill *0.15:bill*0.2;
-const totalValue=bill+tip;
-console.log(bill,tip,totalValue);
